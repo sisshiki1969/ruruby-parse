93e1f72d6cb7f9996bfad741adc247496672ed8f
Soichiro Isshiki <sisshiki@mac.com>
2023-05-04 19:21:39 +0900

    Remove id_table.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,11 @@
 #![feature(const_option)]
 mod error;
-mod id_table;
 mod lvar_collector;
 mod node;
 mod parser;
 mod source_info;
 mod token;
 pub use error::*;
-pub use id_table::*;
 pub use lvar_collector::*;
 pub use node::*;
 pub use parser::*;
